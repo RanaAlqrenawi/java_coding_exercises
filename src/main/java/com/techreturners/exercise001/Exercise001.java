a0a478c5c1d0c0c4ffb101872189e7f874b61b19
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-02-13 21:01:46 +0000

    tidy up exercise one and update exercise two to have less code by using the function equals that return a boolean so no need to use if condition

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -6,12 +6,10 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
         return Character.toUpperCase(word.charAt(0))+word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
         return firstName.charAt(0)+"."+lastName.charAt(0);
     }
 
--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,19 +3,11 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester(Person person) {       
-
-            if(person.getCity().contains("Manchester"))
-                    return true;
-                else
-                    return false;
+            return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
-  
-        if(person.getAge() < ageLimit)
-            return false;
-            else
-                return true;
+        return person.getAge() >= ageLimit;
     }
     
 }
